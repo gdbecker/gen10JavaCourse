5b28ce2f5b0a4dc62956f4ec3383cc14c022df17
Garrett Becker <garrettdbecker@gmail.com>
2019-12-02 13:40:24 -0500

    Updates

---
+++ Milestone 1 - Java Basics/HelloWorld/src/main/java/Hello.java
@@ -0,0 +1,13 @@
+/**
+ *
+ * @author Garrett Becker
+ */
+
+package gdb.HelloWorld; //my Group ID
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("My name is Garrett");
+    }
+}
