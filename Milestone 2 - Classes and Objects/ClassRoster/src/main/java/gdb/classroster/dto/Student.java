dc0f66a282e9363aec65eff2167c6787af370d5f
Garrett Becker <garrettdbecker@gmail.com>
2019-12-17 08:41:49 -0500

    12/16/2019 Updates

--- Milestone 2 - Classes and Objects/ClassRoster/src/main/java/gdb/classroster/dto/Student.java
+++ Milestone 2 - Classes and Objects/ClassRoster/src/main/java/gdb/classroster/dto/Student.java
@@ -1,7 +1,10 @@
 package gdb.classroster.dto;
 
+import java.util.Objects;
+
 /**
  * @date Thursday December 12, 2019
+ * Monday December 16, 2019
  * @author garrettbecker
  */
 
@@ -44,4 +47,43 @@ public class Student {
     public void setCohort(String cohort) {
         this.cohort = cohort;
     }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 29 * hash + Objects.hashCode(this.firstName);
+        hash = 29 * hash + Objects.hashCode(this.lastName);
+        hash = 29 * hash + Objects.hashCode(this.studentId);
+        hash = 29 * hash + Objects.hashCode(this.cohort);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Student other = (Student) obj;
+        if (!Objects.equals(this.firstName, other.firstName)) {
+            return false;
+        }
+        if (!Objects.equals(this.lastName, other.lastName)) {
+            return false;
+        }
+        if (!Objects.equals(this.studentId, other.studentId)) {
+            return false;
+        }
+        if (!Objects.equals(this.cohort, other.cohort)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
 }
---
+++ Section03UnitTests/src/main/java/gdb/section03unittests/SayHi.java
@@ -0,0 +1,14 @@
+package gdb.section03unittests;
+
+/**
+ * @date Monday December 16, 2019
+ * @author garrettbecker
+ */
+
+public class SayHi {
+    
+    //Given a String name, like "Bob," output a greeting with that name
+    public String sayHi(String name) {
+        return "Hello " + name + "!";
+    }
+}
