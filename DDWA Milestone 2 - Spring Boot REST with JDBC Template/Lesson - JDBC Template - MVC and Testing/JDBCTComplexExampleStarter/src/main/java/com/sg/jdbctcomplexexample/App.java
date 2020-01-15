bfdb563b20553f8df41a1e353bb32475fb972f73
Garrett Becker <garrettdbecker@gmail.com>
2020-01-15 15:46:56 -0500

    1/15/2020 Updates

---
+++ DDWA Milestone 2 - Spring Boot REST with JDBC Template/Lesson - JDBC Template - MVC and Testing/JDBCTComplexExampleStarter/src/main/java/com/sg/jdbctcomplexexample/App.java
@@ -0,0 +1,31 @@
+package com.sg.jdbctcomplexexample;
+
+import com.sg.jdbctcomplexexample.controller.MeetingController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+/**
+ *
+ * @author garrettbecker
+ */
+@SpringBootApplication
+public class App implements CommandLineRunner {
+    
+    @Autowired
+    MeetingController controller;
+
+    public static void main(String args[]) {
+        SpringApplication.run(App.class, args);
+    }
+    
+    @Override
+    public void run(String... args) throws Exception {
+        controller.run();
+    }
+}
---
+++ DDWA Milestone 2 - Spring Boot REST with JDBC Template/Lesson - JDBC Template - MVC and Testing/JDBCTComplexExampleStarter/src/main/java/com/sg/jdbctcomplexexample/TestApplicationConfiguration.java
@@ -0,0 +1,20 @@
+package com.sg.jdbctcomplexexample;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+/**
+ * @date Wednesday January 15, 2020
+ * @author garrettbecker
+ */
+
+@Configuration
+    @ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, 
+        value = CommandLineRunner.class))
+    @EnableAutoConfiguration
+public class TestApplicationConfiguration {
+    
+}
